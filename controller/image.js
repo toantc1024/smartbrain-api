78436a63f8244e327cda05274f4c2be9c5ddc598
ghelix2004 <thanhnoobs@gmail.com>
2023-01-11 15:28:24 +0700

    gRPC

--- controller/image.js
+++ controller/image.js
@@ -15,38 +15,67 @@ const handleEntries = (req, res, db) => {
     .catch(err => console.log('unable to get entries'));
 }
 const handleImageUrl = (req, res) => {
-  const raw = JSON.stringify({
-    "user_app_id": {
-      "user_id": "toantc1024",
-      "app_id": "smart-brain"
+  const {ClarifaiStub, grpc} = require("clarifai-nodejs-grpc");
+
+  const stub = ClarifaiStub.grpc();
+
+  const metadata = new grpc.Metadata();
+  metadata.set("authorization", "Key 71ad4ab9e6434aec8b072bab612c341f");
+  stub.PostModelOutputs(
+    {
+        // This is the model ID of a publicly available General model. You may use any other public or custom model ID.
+        model_id: "face-detection",
+        inputs: [{data: {image: {url: req.body.imageUrl}}}]
     },
-    "inputs": [
-        {
-            "data": {
-                "image": {
-                    "url": req.body.imageUrl
-                }
-            }
+    metadata,
+    (err, response) => {
+        if (err) {
+            console.log("Error: " + err);
+            res.json("Error Clarifai gRPC")
+            return;
         }
-    ]
-  });
+
+        if (response.status.code !== 10000) {
+            console.log("Received failed status: " + response.status.description + "\n" + response.status.details);
+            res.json("Error Clarifai gRPC")
+            return;
+        }
+        res.json(response);
+    }
+);
+  // const raw = JSON.stringify({
+  //   "user_app_id": {
+  //     "user_id": "toantc1024",
+  //     "app_id": "smart-brain"
+  //   },
+  //   "inputs": [
+  //       {
+  //           "data": {
+  //               "image": {
+  //                   "url": req.body.imageUrl
+  //               }
+  //           }
+  //       }
+  //   ]
+  // });
   
-  const requestOptions = {
-      method: 'POST',
-      headers: {
-          'Accept': 'application/json',
-          'Authorization': 'Key ' + '71ad4ab9e6434aec8b072bab612c341f'
-      },
-      body: JSON.stringify(raw)
-  };
-  try {
-    fetch(`https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs`, requestOptions)
-    .then(response => response.text())
-    .then(result => res.json(result))
-    .catch(err => res.json('unable to connect clarifai'));
-  } catch (err) {
-    res.json({err:err});
-  }
+  // const requestOptions = {
+  //     method: 'POST',
+  //     headers: {
+  //         'Accept': 'application/json',
+  //         'Authorization': 'Key ' + '71ad4ab9e6434aec8b072bab612c341f'
+  //     },
+  //     body: JSON.stringify(raw)
+  // };
+  // try {
+    
+  //   fetch(`https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs`, requestOptions)
+  //   .then(response => response.text())
+  //   .then(result => res.json(result))
+  //   .catch(err => res.json('unable to connect clarifai'));
+  // } catch (err) {
+  //   res.json({err:err});
+  // }
 
 }
 
